55933f784f7b332bf42c7ce9971425127def06ca
Snehal <nanavaresnehal115@gmail.com>
2022-11-04 08:19:20 +0530

    ADDED[Snehal]- UC2 -Display BridgeLabz Logo using Property Binding

--- HelloWorld-AngularApp/src/app/app.component.ts
+++ HelloWorld-AngularApp/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld-AngularApp';
 
+  imgUrl="../assets/BridgeLabzLogo.jpg";
+
   /* Initializing title when ngOnInit lifecycle event is fired.*/
   ngOnInit(): void{
     this.title = "Hello From BridgeLabz."
